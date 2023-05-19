e64a4e86b0760e1bf307225271c5d92f80700394
BCorbin825 <73535716+BCorbin825@users.noreply.github.com>
2023-05-19 00:15:12 -0400

    Added highlighting to nav links

--- script.js
+++ script.js
@@ -1,3 +1,10 @@
+const navigationElements = document.querySelectorAll('#nav-list li');
+
+const overview_nav = document.getElementById("overview-nav");
+const features_nav = document.getElementById("features-nav");
+const how_it_works_nav = document.getElementById("how-it-works-nav");
+const tech_specs_nav = document.getElementById("tech-specs-nav");
+
 const gold_btn = document.getElementById("gold-button");
 const black_btn = document.getElementById("black-button");
 const white_btn = document.getElementById("white-button");
@@ -5,6 +12,39 @@ const white_btn = document.getElementById("white-button");
 const gold_img = document.getElementById("gold");
 const black_img = document.getElementById("black");
 const white_img = document.getElementById("white");
+
+window.addEventListener('scroll', () => {
+    // Get the current scroll position and viewport height
+    const scrollPosition = window.scrollY;
+    const viewportHeight = window.innerHeight;
+  
+    // Find the section that is currently taking up the most of the viewport
+    let maxVisibleArea = 0;
+    let activeSectionId = '';
+  
+    // Iterate over each section
+    document.querySelectorAll('section').forEach(section => {
+      // Get the offset top, height, and visible area of the section
+      const sectionTop = section.offsetTop;
+      const sectionHeight = section.offsetHeight;
+      const visibleArea = Math.min(scrollPosition + viewportHeight, sectionTop + sectionHeight) - Math.max(scrollPosition, sectionTop);
+  
+      // Update the active section if the current section has more visible area
+      if (visibleArea > maxVisibleArea) {
+        maxVisibleArea = visibleArea;
+        activeSectionId = section.getAttribute('id');
+      }
+    });
+  
+    // Find the corresponding navigation element and add the highlight class
+    const activeNavElement = document.querySelector(`#nav-list li a[href="#${activeSectionId}"]`);
+    if (activeNavElement) {
+      navigationElements.forEach(element => {
+        element.querySelector('a').classList.remove('highlight');
+      });
+      activeNavElement.classList.add('highlight');
+    }
+  });
 
 gold_btn.addEventListener("click", function() {
     gold_img.classList.remove("hidden");
