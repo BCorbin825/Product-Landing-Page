513f4e254581d37c5c2e97f80884dbbc270474e4
BCorbin825 <73535716+BCorbin825@users.noreply.github.com>
2023-05-19 19:27:11 -0400

    More changes

--- script.js
+++ script.js
@@ -14,37 +14,49 @@ const black_img = document.getElementById("black");
 const white_img = document.getElementById("white");
 
 window.addEventListener('scroll', () => {
-    // Get the current scroll position and viewport height
-    const scrollPosition = window.scrollY;
-    const viewportHeight = window.innerHeight;
-  
-    // Find the section that is currently taking up the most of the viewport
-    let maxVisibleArea = 0;
-    let activeSectionId = '';
-  
-    // Iterate over each section
-    document.querySelectorAll('section').forEach(section => {
-      // Get the offset top, height, and visible area of the section
-      const sectionTop = section.offsetTop;
-      const sectionHeight = section.offsetHeight;
-      const visibleArea = Math.min(scrollPosition + viewportHeight, sectionTop + sectionHeight) - Math.max(scrollPosition, sectionTop);
-  
-      // Update the active section if the current section has more visible area
-      if (visibleArea > maxVisibleArea) {
-        maxVisibleArea = visibleArea;
-        activeSectionId = section.getAttribute('id');
-      }
-    });
-  
-    // Find the corresponding navigation element and add the highlight class
-    const activeNavElement = document.querySelector(`#nav-list li a[href="#${activeSectionId}"]`);
-    if (activeNavElement) {
-      navigationElements.forEach(element => {
-        element.querySelector('a').classList.remove('highlight');
-      });
-      activeNavElement.classList.add('highlight');
+  // Get the current scroll position and viewport height
+  const scrollPosition = window.scrollY;
+  const viewportHeight = window.innerHeight;
+
+  // Find the section that is currently taking up the most of the viewport
+  let maxVisibleArea = 0;
+  let activeSectionId = '';
+
+  // Iterate over each section
+  document.querySelectorAll('section').forEach(section => {
+    // Get the offset top, height, and visible area of the section
+    const sectionTop = section.offsetTop;
+    const sectionHeight = section.offsetHeight;
+    const visibleArea = Math.min(scrollPosition + viewportHeight, sectionTop + sectionHeight) - Math.max(scrollPosition, sectionTop);
+
+    // Update the active section if the current section has more visible area
+    if (visibleArea > maxVisibleArea) {
+      maxVisibleArea = visibleArea;
+      activeSectionId = section.getAttribute('id');
     }
   });
+
+  // Find the corresponding navigation element and add the highlight class
+  const activeNavElement = document.querySelector(`#nav-list li a[href="#${activeSectionId}"]`);
+  if (activeNavElement) {
+    navigationElements.forEach(element => {
+      element.querySelector('a').classList.remove('highlight');
+    });
+    activeNavElement.classList.add('highlight');
+  }
+});
+
+window.addEventListener('resize', function() {
+  const menu_icon = document.getElementById("menu-icon");
+  var screenWidth = window.innerWidth;
+
+  if (screenWidth <= 1000) {
+    menu_icon.classList.remove("hidden");
+  }
+  else if (screenWidth > 1000) {
+    menu_icon.classList.add("hidden");
+  }
+});
 
 gold_btn.addEventListener("click", function() {
     gold_img.classList.remove("hidden");
